85a6dcb743e2c47cb741df4f0c456086850c0979
Hachimmia <92265991+Hachimmia@users.noreply.github.com>
2023-01-24 19:04:08 +0000

    Project upload
    upload of the whole project

---
+++ src/animations.js
@@ -0,0 +1,38 @@
+const animatedItems = document.querySelectorAll(".animated");
+const animatedItemsUp = document.querySelectorAll(".animatedUp");
+
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			entry.target.classList.toggle("animate__animated", entry.isIntersecting);
+			entry.target.classList.toggle("animate__fadeIn", entry.isIntersecting);
+			if (entry.isIntersecting) observer.unobserve(entry.target);
+		});
+	},
+	{
+		threshold: 1,
+	}
+);
+
+animatedItems.forEach((item) => {
+	observer.observe(item);
+});
+
+const observerUp = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			entry.target.classList.toggle("animate__animated", entry.isIntersecting);
+			entry.target.classList.toggle("animate__fadeInUp", entry.isIntersecting);
+			if (entry.isIntersecting) {
+				observerUp.unobserve(entry.target);
+			}
+		});
+	},
+	{
+		threshold: 1,
+	}
+);
+
+animatedItemsUp.forEach((item) => {
+	observerUp.observe(item);
+});
